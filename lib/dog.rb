56d0aa2968000f59d60fe73d772d81bb66f668bb
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-18 15:32:45 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+  
+  def name=(dog_name) # Setter Method for the dogs name
+    this_dogs_name = dog_name
+  end
+  
+  def name  # Getter Method for the dogs name
+    this_dogs_name
+  end
+  
+end
